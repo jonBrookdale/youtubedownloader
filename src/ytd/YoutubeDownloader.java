88b53066186e401b85eb635bfb90085ac4a7aecd
jonBrookdale <jonathan@thebaileys.me.uk>
2015-07-11 17:22:56 +0100

    Updated build.xml and added Log4j
    Updated the build.xml to include testing, and also added Log4j into the
    project

--- src/ytd/YoutubeDownloader.java
+++ src/ytd/YoutubeDownloader.java
@@ -1,14 +1,24 @@
+package ytd;
+
 import javax.swing.JFrame;
 import javax.swing.UIManager;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.BasicConfigurator;
+
 public class YoutubeDownloader {
+	static Logger logger = Logger.getLogger(YoutubeDownloader.class);
+	
 	public static void main(String[] args) {
+		BasicConfigurator.configure();
+		logger.info("Hello World");
+		
 		try {
 			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 		} catch (Exception e) {
-			
+			logger.info("This system is not supported");
 		}
-		
+
 		JFrame mainWindow = new JFrame("Youtube Downloader");
 		int windowWidth = 400;           // Window width in pixels
 		int windowHeight = 150;          // Window height in pixels
---
+++ test/YoutubeDownloaderTest.java
@@ -0,0 +1,12 @@
+public class YoutubeDownloaderTest extends junit.framework.Testcase {
+	public void testNothing() {
+		}
+		
+		public void testWillAlwaysFail() {
+			fail("An error message");
+		}
+		
+		public void testWillAlwaysPass() {
+			pass();
+		}
+}
